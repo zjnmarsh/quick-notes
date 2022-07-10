d367306df4c720b7fd1d7d55c14215169859d980
Zebedee Marsh <zjnmarsh@gmail.com>
2022-07-10 14:23:28 +0100

    basic functionality of adding and deleting notes. not persistent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import '@fontsource/roboto/300.css'
+//
+import {Box, Button, Grid, TextField, Typography} from "@mui/material";
+import NoteCard from './NoteCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+    const deleteNote = (key) => {
+        console.log(key);
+        let noteIndex = notes.findIndex(note => note.key === key);
+        let newArr1 = notes.slice(0, noteIndex);
+        let newArr2 = notes.slice(noteIndex+1, notes.length);
+        newArr1.concat(newArr2);
+        setNotes(newArr1);
+    }
+
+    // const [notes, setNotes] = useState([{text: "This is the first note", date: "", key: 0}, {text: "This is a second note", date: "", key: 1}, {text: "This is a third note", date: "", key: 2}])
+    const [notes, setNotes] = useState([]);
+    const [newNote, setNewNote] = useState("");
+
+    const addNewNote = () => {
+        setNotes(notes.concat([{text: newNote, date:"", key: Date()}]));
+        setNewNote("");
+    }
+
+    return (<div className="App">
+        <Box>
+            <Box>
+                <Typography variant={"h2"} component={"div"} gutterBottom>
+                    Quick notes
+                </Typography>
+            </Box>
+            <Grid container spacing={2}>
+                {/*<NoteCard data={{text: "Hello", date: "14-01-2022"}} />*/}
+                {
+                    notes.map((note) => (
+                        <NoteCard data={note} key={note.key} deletefunc={deleteNote}/>
+                    ))
+                }
+            </Grid>
+
+            <Box
+                sx={{
+                    display: 'flex',
+                    height: '5rem',
+                    width: '100%',
+                    position: 'fixed',
+                    bottom: '0px',
+                }}>
+                <Box
+                    sx={{
+                        // border: '3px solid #8AC007',
+                        width: '70%',
+                        margin: 'auto'
+                    }}>
+                    <TextField
+                        id={"input-box"}
+                        label={"quick note"}
+                        multiline
+                        rows={1}
+                        fullWidth
+                        value={newNote}
+                        variant={"filled"}
+                        onChange={(e) => {setNewNote(e.target.value)}}
+                        onKeyPress={(e) => {
+                            if (e.key === 'Enter'){
+                                addNewNote()
+                            }
+                        }}
+                    />
+                </Box>
+                <Box
+                    id={"add-note-button"}
+                    display={"flex"}
+                    justifyContent={"center"}
+                    alignItems={"center"}
+                    width={"30%"}>
+                    <Button
+                        variant={"contained"}
+                        onClick={() => addNewNote()}
+                    >
+                        Enter</Button>
+                </Box>
+            </Box>
+        </Box>
+
+
+    </div>)
 }
 
 export default App;
---
+++ src/NoteCard.jsx
@@ -0,0 +1,64 @@
+import {
+    Button,
+    Card,
+    CardActionArea,
+    CardContent,
+    Dialog, DialogActions,
+    DialogContent,
+    DialogContentText,
+    Grid,
+    Typography
+} from "@mui/material";
+import React, {useState} from 'react';
+
+const NoteCard = ({data, deletefunc}) => {
+
+    const [alertOpen, setAlertOpen] = useState(false);
+
+    const handleOpen = () => {
+        setAlertOpen(true);
+    }
+
+    const handleClose = () => {
+        setAlertOpen(false);
+    }
+
+    const deleteMessage = () => {
+        // delete the message
+        deletefunc(data.key)
+    }
+
+    return (
+        <Grid item xs={12} sm={6} md={4} >
+            <Card>
+                <CardActionArea onClick={handleOpen}>
+                    <CardContent>
+                        <Typography variant={"body1"}>
+                            {data.text}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <Dialog
+                    open={alertOpen}
+                    onClose={handleClose}
+                >
+                    <DialogContent>
+                        <DialogContentText>
+                            Delete Note?
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={deleteMessage}>
+                           Delete
+                        </Button>
+                        <Button onClick={handleClose}>
+                            Cancel
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </Card>
+        </Grid>
+    )
+}
+
+export default NoteCard
